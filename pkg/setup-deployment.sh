2a415711dd6fc5080c6f74546354794997e0d45c
Ryan Reich <ryan.reich@gmail.com>
2016-10-13 12:54:32 -0700

    Add solc build

--- pkg/setup-deployment.sh
+++ pkg/setup-deployment.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-apt-get -y install curl
-curl -sL https://deb.nodesource.com/setup_6.x | bash -
-apt-get -y install libleveldb-dev libpq-dev libpcre3-dev nodejs
+set -e
 
+apt-get -y install \
+  curl libleveldb-dev libpq-dev libpcre3-dev \
+  libboost-all-dev libjsoncpp-dev libstdc++6
+curl -sL https://deb.nodesource.com/setup_6.x | bash -
+apt-get -y install nodejs 
