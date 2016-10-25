a49646676166ae27bf5efdf1f5888747d09ac156
Ryan Reich <ryan@blockapps.net>
2016-10-25 21:12:05 +0000

    Add setup-build.sh

--- pkg/setup-build.sh
+++ pkg/setup-build.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
-apt-get -y install \
-  postgresql postgresql-server-dev-9.5 libleveldb-dev \
-  libpcre3-dev libpcre++-dev curl netcat
-curl -sSL https://get.haskellstack.org/ | sh
+set -e
+
+$sudo apt-get -y install \
+  lsb-release \
+  curl libleveldb-dev libpq-dev libpcre3-dev \
+  libboost-all-dev libjsoncpp-dev libstdc++6
+$sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 575159689BEFB442
+echo "deb http://download.fpcomplete.com/ubuntu $(lsb_release -s -c) main" | \
+  sudo tee /etc/apt/sources.list.d/fpco.list
+$sudo apt-get update
+$sudo apt-get -y install stack
+sed -i 's/resolver:.*/resolver: lts-3.4/' ~/.stack/global-project/stack.yaml
+stack setup
 stack install alex happy
