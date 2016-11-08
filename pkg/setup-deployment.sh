c5b5f03f92782ee2f60331f0b9fc6921bc4e78d9
Ryan Reich <ryan.reich@gmail.com>
2016-11-08 09:42:06 -0800

    Add sudo to setup-deployment.sh

--- pkg/setup-deployment.sh
+++ pkg/setup-deployment.sh
@@ -2,9 +2,9 @@
 
 set -e
 
-apt-get -y install \
+$sudo apt-get -y install \
   curl libleveldb-dev libpq-dev libpcre3-dev \
   libboost-all-dev libjsoncpp-dev libstdc++6 \
   netcat-openbsd netbase
-curl -sL https://deb.nodesource.com/setup_6.x | bash -
-apt-get -y install nodejs 
+curl -sL https://deb.nodesource.com/setup_6.x | $sudo bash -
+$sudo apt-get -y install nodejs 
