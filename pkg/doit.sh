7a269f1ffab617bc780adfffc0fc9279ade956a0
kejace <kejace@gmail.com>
2016-12-09 17:37:10 +0000

    Fixed the miningThread parameter typo

--- pkg/doit.sh
+++ pkg/doit.sh
@@ -15,7 +15,7 @@ function newnode {
   if $mineBlocks
   then echo "Starting strato-adit and strato-quarry"
       export miningThreads=${miningThreads:-1}
-      runForever strato-adit --threads=$miningThreads:-1} --aMiner=$miningAlgorithm >> logs/strato-adit 2>&1
+      runForever strato-adit --threads=${miningThreads:-1} --aMiner=$miningAlgorithm >> logs/strato-adit 2>&1
        runForever strato-quarry >> logs/strato-quarry 2>&1
   fi
 
